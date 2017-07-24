663a17bbe6d9cd88ac9ea5377f741e90ecfc8d2f
Bahman Masarratbakhsh <atropatnet@yahoo.com>
2017-07-23 19:57:22 -0700

    simple function

---
+++ Thread/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Function1();
+            Function2();
+            Console.ReadKey();
+        }
+        static private void Function1()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Function1 i={i}");
+            }
+        }
+
+        static private void Function2()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Function2 i={i}");
+            }
+        }
+
+    }
+}
