99838102aeb203e42d746b576d27720f3de5bf3a
Bahman Masarratbakhsh <atropatnet@yahoo.com>
2017-07-23 18:57:16 -0700

    MVC part

---
+++ MVCAsync/Controllers/AsyncController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http;
+
+namespace MVCAsync.Controllers
+{
+    public class AsyncController : Controller
+    {
+        // GET: Async
+        public ActionResult Sync()
+        {
+            return View();
+        }
+
+        DownloadData(string url)
+        {
+            var httpClinet = new HttpClient();
+            var httpResponseMessage = httpClinet.GetAsync(url).Result;
+            httpResponseMessage.EnsureSuccessStatusCode();
+            httpResponseMessage.Content.ReadAsAsync<IEnumerable<Video>>().Result;
+        }
+
+    }
+}
---
+++ MVCAsync/Models/Video.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCAsync.Models
+{
+    public class Video
+    {
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public string ImageUrl { get; set; }
+
+    }
+}
